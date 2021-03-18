81695d47e60bd5594012fad9322a0959fabfd6f6
Damien Dorobek <ddorobek@uwo.ca>
2021-03-17 21:03:14 -0400

    Set up pub/sub model in server.js Set up WebSocket between front-end and back-end
    Messages are duplicating... needs to be fixed

--- 4471-stocks-ui/src/App.js
+++ 4471-stocks-ui/src/App.js
@@ -1,22 +1,168 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import logo from './logo192.png';
 import './App.css';
 
+import { TextField, Button, CircularProgress } from '@material-ui/core';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+
 function App() {
+
+    const ws = new WebSocket("ws://localhost:8080/");
+    const channel = ["stock-listings", "stock-performance", "stock-compare"]
+
+    const [subscribed, setSubscribed] = useState(false)
+    const [tickers, setTickers] = useState(null)
+    const [tickerValue, setTickerValue] = useState(null)
+    const [stockDate, setStockDate] = useState('')
+    const [stock, setStock] = useState(null)
+
+
+    /*ws.onmessage = (evt) => {
+        let message = JSON.parse(evt.data)
+        console.log(message)
+        if (message.channel == channel[0] && message.message == "subscribe") {
+            setSubscribed([true, subscribed[1], subscribed[2]])
+        }
+        if (message.channel == channel[1] && message.message == "subscribe") {
+            setSubscribed([subscribed[0], true, subscribed[2]])
+        }
+        if (message.channel == channel[2] && message.message == "subscribe") {
+            setSubscribed([subscribed[0], subscribed[1], true])
+        }
+        if (message.channel == channel[0] && message.message != "subscribe") {
+            setTickers(message.message)
+        }
+
+    };*/
+
+    useEffect(() => {
+        ws.onopen = () => {
+            console.log('WebSocket Client Connected');
+            ws.send(JSON.stringify({ channel: channel[0], message: 'subscribe' }))
+            ws.send(JSON.stringify({ channel: channel[1], message: 'subscribe' }))
+            ws.send(JSON.stringify({ channel: channel[2], message: 'subscribe' }))
+        };
+
+        ws.onmessage = (evt) => {
+            let message = JSON.parse(evt.data)
+            console.log(message)
+            if (message.channel == channel[2] && message.message == "subscribed") {
+                setSubscribed(true)
+                ws.close()
+            }
+            
+
+        };
+
+    }, []);
+
+    useEffect(() => {
+        if (subscribed) {
+            console.log('subscribed!')
+            let body = { channel: 'stock-listings', message: 'get' }
+            ws.onopen = () => {
+                ws.send(JSON.stringify(body)) 
+            };
+
+            ws.onmessage = (evt) => {
+                let message = JSON.parse(evt.data)
+                console.log(message)
+                if (message.channel == channel[0] && message.message != "subscribed") {
+                    setTickers(message.message)
+                    ws.close()
+                }
+
+            };
+        }
+        
+    }, [subscribed])
+
+    const getStockInfo = () => {
+        let body = {
+            channel: channel[1],
+            message: { value: tickerValue, date: stockDate }
+        }
+        console.log(body)
+        ws.send(JSON.stringify(body))
+        ws.onmessage = (evt) => {
+            let message = JSON.parse(evt.data)
+            setStock(message)
+        };
+    }
+
+    useEffect(() => {
+        console.log('stock', stock)
+    }, [stock])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+                  Stock Tracker
+        </h1>
+        <div className="Container">
+            {
+                tickers != null
+                    ? <>
+                        <Autocomplete
+                            className="TextField"
+                            options={tickers}
+                            value={tickerValue}
+                            onChange={(event, newValue) => {
+                                setTickerValue(newValue);
+                            }}
+                            color="primary"
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Choose a ticker"
+                                    variant="outlined"
+                                    inputProps={{
+                                        ...params.inputProps,
+                                    }}
+                                />
+                            )}
+                        />
+                        {tickerValue != null
+                                  ? <>
+                                      <TextField className="TextField" label="Enter date" variant="outlined" color="primary" placeholder="YYYYMMDD" value={stockDate} onChange={(e) => setStockDate(e.target.value)} />
+                                      <Button fullWidth variant="contained" color="primary" onClick={() => getStockInfo()}>See Stock</Button>
+                              </>
+                              : null
+                        }
+                        {stock == null
+                            ? null
+                            : <>
+                                <p>{tickerValue} Stock</p>
+                                      <LineChart
+                                          width={500}
+                                          height={300}
+                                          data={stock.message}
+                                          margin={{
+                                              top: 5,
+                                              right: 30,
+                                              left: 20,
+                                              bottom: 5,
+                                          }}
+                                      >
+                                          <CartesianGrid strokeDasharray="3 3" />
+                                          <XAxis dataKey="price" />
+                                          <YAxis domain={['dataMin', 'dataMax']}/>
+                                          <Tooltip />
+                                          <Legend />
+                                          <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} />
+                                          {/*<Line type="monotone" dataKey="uv" stroke="#82ca9d" />*/}
+                                      </LineChart>
+                                
+                            </>
+                        }
+                    </>
+                    : null
+            }
+        </div> 
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,14 +1,111 @@
-const http = require('http');
+const express = require('express');
+const redis = require('redis');
+const WebSocket = require('ws');
+const mysql = require('mysql');
 
-const hostname = '127.0.0.1';
-const port = 4000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const app = express();
+const pubURL = 'http://localhost:3005'
+
+const subscriber = redis.createClient();
+//console.log(subscriber)
+const publisher = redis.createClient();
+const wss = new WebSocket.Server({ port: 8080 });
+
+const channel = ["stock-listings", "stock-performance", "stock-compare"]
+
+const conCredentials = {
+    host: "stocks-database.cp0mxw253ygq.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "5tYtKvTwF4ZqsdgAmwaM"
+}
+
+wss.on('connection', function connection(ws) {
+    ws.onmessage = (evt) => {
+        let message = JSON.parse(evt.data)
+
+        console.log(message)
+        if (message.message == "subscribe") {
+            channel.forEach(channel => {
+                if (message.channel == channel) {
+                    subscriber.subscribe(channel);
+                }
+            })
+            
+        } else {
+            if (message.channel == channel[0]) {
+                query = 'SELECT DISTINCT symbol FROM Temp_Stocks.tickers;'
+                let con = mysql.createConnection(conCredentials);
+
+                con.connect(function (err) {
+                    if (err) throw err;
+                    console.log("Connected to DB!");
+
+                    con.query(query, function (err, result) {
+                        if (err) throw err;
+                        let tickers = Object.values(result).map(res => res.symbol)
+                        message = { ...message, message: tickers }
+                        //publisher.publish(channel[0], {message: 'sadsaddsad' });
+                        console.log('publishing...')
+                        publisher.publish(channel[0], JSON.stringify(message));
+                        //ws.send((JSON.stringify({ channel: message.channel, message: message.message, status: 200 })))
+                    });
+                    con.end()
+                });
+
+                //get tickers from sql
+                
+            }
+            if (message.channel == channel[1]) {
+                query = 'SELECT * FROM Stocks.' + message.message.value + '_stock WHERE date = ' + message.message.date + ';'
+                let con = mysql.createConnection(conCredentials);
+
+                con.connect(function (err) {
+                    if (err) throw err;
+                    console.log("Connected to DB!");
+
+                    con.query(query, function (err, result) {
+                        if (err) throw err;
+                        let stock = Object.values(result).filter((res, i) => i % 25 == 0)
+
+                        //console.log(stock)
+                        message = { ...message, message: stock }
+                        //publisher.publish(channel[0], {message: 'sadsaddsad' });
+                        console.log('publishing...')
+                        publisher.publish(channel[1], JSON.stringify(message));
+                        //ws.send((JSON.stringify({ channel: message.channel, message: message.message, status: 200 })))*/
+                    });
+                    con.end()
+                });
+            }
+            if (message.channel == channel[2]) {
+                publisher.publish(channel[2], 'hello3');
+            }
+            
+        }
+        console.log('received: ', message.channel, message.message);
+    };
+
+    subscriber.on("message", (channel, message) => {
+        console.log('subscriber received message ... ', channel)
+        ws.send(message)
+
+        //returned data from publisher
+    })
+
+    subscriber.on('subscribe', function (channel, count) {
+        ws.send(JSON.stringify({ channel: channel, message: 'subscribed' }))
+        console.log('Subscribed to channel: ' + channel)
+    });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+
+
+
+
+
+
+app.listen(3006,() => {
+    console.log("server is listening to port 3006");
+})
